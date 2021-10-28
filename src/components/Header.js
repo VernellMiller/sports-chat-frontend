9698aab6438f6447be3d6251f101650a9a7386dc
vernellmiller <nezeey@gmail.com>
2021-10-28 19:41:15 -0400

    wip adds link to login and logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,48 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = (props) => {
+
+    const StyledHeader = styled.header`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-left: 1rem;
+        height: 5rem;
+        background-color: black, 0.5;
+        
+        nav {
+            display: flex;
+            align-items: center;
+            ul {
+                display: flex;
+                list-style: none;
+                li {
+                    margin-right: 1.5rem;
+                    a {
+                        text-decoration: none;
+                        color: inherit;
+                    }
+                    &:last-child:hover {
+                        cursor: pointer;
+                    }
+                }
+            }
+        }
+    `;
+
     return (
-        <header>
+        <StyledHeader>
             <h1>Header</h1>
-        </header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link>Login</Link>
+                    </li>
+                    <li>Logout</li>
+                </ul>
+            </nav>
+        </StyledHeader>
     );
 };
 
