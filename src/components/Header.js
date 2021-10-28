7b1850802aa183ea76daa293262933132f69805f
vernellmiller <nezeey@gmail.com>
2021-10-28 19:55:29 -0400

    wip: moves styles from component to styles.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,10 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+// import styled from "styled-components";
+import { logOut } from "../services/firebase";
+import { StyledHeader } from "../styles.js";
+
 
 const Header = (props) => {
-
-    const StyledHeader = styled.header`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding-left: 1rem;
-        height: 5rem;
-        background-color: black, 0.5;
-        
-        nav {
-            display: flex;
-            align-items: center;
-            ul {
-                display: flex;
-                list-style: none;
-                li {
-                    margin-right: 1.5rem;
-                    a {
-                        text-decoration: none;
-                        color: inherit;
-                    }
-                    &:last-child:hover {
-                        cursor: pointer;
-                    }
-                }
-            }
-        }
-    `;
-
     return (
         <StyledHeader>
             <h1>Header</h1>
@@ -39,7 +13,7 @@ const Header = (props) => {
                     <li>
                         <Link>Login</Link>
                     </li>
-                    <li>Logout</li>
+                    <li onClick={ logOut }>Logout</li>
                 </ul>
             </nav>
         </StyledHeader>
---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 1rem;
+    height: 5rem;
+    background-color: black, 0.5;
+
+    nav {
+        display: flex;
+        align-items: center;
+        ul {
+            display: flex;
+            list-style: none;
+            li {
+                margin-right: 1.5rem;
+                font-size: 0.7rem;
+                font-weight: 700;
+                a {
+                    text-decoration: none;
+                    color: inherit;
+                }
+                &:last-child:hover {
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+`;
