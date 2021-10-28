2c1e548e05e0f2e327235c92801f270b6b3dc884
vernellmiller <nezeey@gmail.com>
2021-10-28 15:57:44 -0400

    wip: adds boilerplate to pages

---
+++ src/pages/Landing.js
@@ -0,0 +1,9 @@
+const Landing = (props) => {
+    return (
+        <main>
+            <h1>Landing</h1>
+        </main>
+    )
+}
+
+export default Landing;
---
+++ src/pages/Main.js
@@ -0,0 +1,9 @@
+const Main = (props) => {
+    return (
+        <main>
+            <h1>Mains</h1>
+        </main>
+    )
+}
+
+export default Main;
