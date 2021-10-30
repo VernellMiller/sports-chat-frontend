dd7b342beeab06919787f09f25998c4db577d7f9
vernellmiller <nezeey@gmail.com>
2021-10-29 22:23:32 -0400

    wip: adds and exports SubMenu component

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,3 +1,16 @@
 import React, { useState } from 'react';
+import { IoMdHeartEmpty } from 'react-icons/Io';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+
+
+const SubMenu = ( ) => {
+    return (
+      <>
+        <SidebarLink to={item.path}></SidebarLink>
+      </>
+    );
+  };
+  
+  export default SubMenu;
