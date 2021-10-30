69cd000886ea431b5c0a9dfd66e0e46fed72a1c4
vernellmiller <nezeey@gmail.com>
2021-10-30 13:22:30 -0400

    wip: adding subMenu

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,29 +1,29 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 
-// const SidebarLink = styled(Link)`
-//   display: flex;
-//   color: #e1e9fc;
+const SidebarLink = styled(Link)`
+  display: flex;
+  color: #e1e9fc;
 
-// `;
+`;
 
-// const SidebarLabel = styled.span`
-//   margin-left: 16px;
-// `;
+const SidebarLabel = styled.span`
+  margin-left: 16px;
+`;
 
-// const SubMenu = ({item}) => {
-//     return (
-//       <>
-//         <SidebarLink to={item.path}>
-//         <div>
-//           {item.icon}
-//           <SidebarLabel>{item.title}</SidebarLabel>
-//         </div>
-//         </SidebarLink>
-//       </>
-//     );
-//   };
+const SubMenu = ({item}) => {
+    return (
+      <>
+        <SidebarLink to={item.path}>
+        <div>
+          {item.icon}
+          <SidebarLabel>{item.title}</SidebarLabel>
+        </div>
+        </SidebarLink>
+      </>
+    );
+  };
   
-//   export default SubMenu;
+  export default SubMenu;
