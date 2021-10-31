02136fde1b57603029037693bf43207b6a55a6dc
vernellmiller <nezeey@gmail.com>
2021-10-31 16:58:49 -0400

    wip: attempts to use firestore

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,6 +3,8 @@ import "firebase/firebase-auth";
 import "firebase/firebase-firestore";
 
 
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDhXy1oVIJR9BaKeBJLPG-WuQSkk6CCZNg",
     authDomain: "sports-chat-d6a85.firebaseapp.com",
@@ -17,6 +19,8 @@ firebase.initializeApp(firebaseConfig);
 // set up auth
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider()
+const db = firebase.firestore();
+
 
 
 function signIn() {
@@ -31,7 +35,8 @@ function logOut() {
 export {
     auth,
     signIn,
-    logOut
+    logOut,
+    db
 }
 
   
