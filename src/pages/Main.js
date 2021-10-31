01ee150fcdf9b44df677a7fb71b9ac2278011e0c
vernellmiller <nezeey@gmail.com>
2021-10-31 00:03:37 -0400

    adds chat component to content

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,13 @@
 import { StyledContent } from "../styles";
+import Chat from "../components/Chat";
 
 const Content = (props) => {
     return (
         <StyledContent>
+            <>
             <h1>Content</h1>
+            <Chat />
+            </>
         </StyledContent>
     );
 };
