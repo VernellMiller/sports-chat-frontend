c0c6d1cd876fa20f3d06d8b241307fde6f8b8a7e
vernellmiller <nezeey@gmail.com>
2021-11-01 16:02:04 -0400

    renders cards in main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,13 @@
 import { StyledContent } from "../styles";
 import Chat from "../components/Chat";
+import Cards from "../components/Cards";
 
 const Content = (props) => {
     return (
         <StyledContent>
             <>
             <h1>Content</h1>
-            <article className="cards">the</article>
+            <Cards />
             <Chat />
             </>
         </StyledContent>
