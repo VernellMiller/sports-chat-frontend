584ad9a460f47fc2d4a98bd87965cc1898780ef0
vernellmiller <nezeey@gmail.com>
2021-11-01 15:58:40 -0400

    adds cards components

---
+++ src/components/Cards.js
@@ -0,0 +1,77 @@
+import { StyledCards } from "../styles";
+
+const Cards = (props) => {
+    return (
+        <StyledCards>
+            <>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                <article>
+                    <>
+                        <h1>image</h1>
+                        <h1>name</h1>
+                        <p>stats</p>
+                    </>
+                </article>
+                
+            
+            </>
+        </StyledCards>
+    );
+};
+
+export default Cards;
