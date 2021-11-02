5239474e8f120204f53f5ee862523f9e8f663e1c
vernellmiller <nezeey@gmail.com>
2021-11-02 11:47:34 -0400

    wip: adds helper functions and returns props

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom";
 import React from 'react'
 
-const index = () => {
-    return (
-        <div>
-            <h1>index</h1>
+const index = (props) => {
+
+const loaded = () => {
+    return props.players.map(player => (
+        <div key={player._id} className="player">
+            {/* <link to={`/players/${player._id}`}>
+            </link> */}
+                <h1>{player.firstName}</h1>
+            <img style={{height: 70}} src={player.image} alt={player.name} />
+            <h3>{player.position}</h3>
         </div>
-    )
-}
+    ));
+};
+
+
+const loading = () => <h3>Loading...</h3>
+
+    return props.players ? loaded() : loading();
+};
 
 export default index
