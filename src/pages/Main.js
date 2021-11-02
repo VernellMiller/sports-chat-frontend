944bc4756fed212da7c3d6725bc4a432d687a8b7
vernellmiller <nezeey@gmail.com>
2021-11-01 21:01:45 -0400

    adds index and show path

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,25 @@
+import { Route, Switch } from "react-router";
 import { StyledContent } from "../styles";
 import Chat from "../components/Chat";
 import Cards from "../components/Cards";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
 
 const Content = (props) => {
     return (
         <StyledContent>
             <>
-            <h1>Content</h1>
-            <Cards />
-            <Chat />
+                <h1>Players</h1>
+            <Switch>
+                <Route exact path="/">
+                    <Index /> 
+                </Route>
+                <Route path="/players/:id" render={(props) => (
+                    <Show {...props} /> 
+                )} >
+
+                </Route>
+            </Switch>
             </>
         </StyledContent>
     );
