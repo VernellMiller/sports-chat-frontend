bb87ae63cd1ab3057252fbd6943809dacc7e0de3
vernellmiller <nezeey@gmail.com>
2021-11-02 17:15:02 -0400

    creates show component

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,11 +1,94 @@
-import React from 'react'
+import { Link, useParams } from "react-router-dom";
+import React, { useState } from 'react';
 
-const show = () => {
-    return (
-        <div>
-            <h1>show</h1>
+
+const Show = (props) => {
+
+let {teamid} = useParams();
+console.log(teamid);
+
+const [newForm, setNewForm ] = useState(getNewState());
+
+const loaded = () => {
+    const teamArray = props.players.filter(player => {
+        return player.team === teamid
+    });
+    
+    return teamArray.map(player => (
+        <div key={player._id} className="player">
+            {/* <link to={`/players/${player._id}`}>
+            </link> */}
+            <img style={{height: 50, borderRadius: "50%"}} src={player.image} alt={player.name} />
+            <h3>{player.firstName} {player.lastName}</h3>
+            <p>{player.position}</p>
         </div>
-    )
-}
+    ));
+};
 
-export default show
+const loading = () => <h3>Loading...</h3>
+// form helper functions
+const handleChange = (event) => {
+    setNewForm(prevState => ({
+        ...prevState,
+        [event.target.name]: event.target.value
+
+        }
+    ))
+};
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const teamForm = {
+        ...newForm, team:teamid
+    }
+    props.createPlayers(teamForm);
+    setNewForm(getNewState());
+};
+
+function getNewState() {
+    return {
+        image: "",
+        firstName: "",
+        lastName: "",
+        position: "",
+    }
+};
+
+    return (
+        <section>
+            <form className="Form" onSubmit={handleSubmit}>
+                <input 
+                value={newForm.image} 
+                onChange={handleChange}
+                type="text"
+                placeholder="Image"
+                name="image" 
+                />
+                <input
+                 value={newForm.firstName} 
+                 onChange={handleChange}
+                 type="text"
+                 placeholder="First Name"
+                 name="firstName" 
+                 />
+                <input
+                 value={newForm.lastName} 
+                 onChange={handleChange}
+                 type="text"
+                 placeholder="Last Name"
+                 name="lastName" 
+                 />
+                <input 
+                value={newForm.position} 
+                onChange={handleChange}
+                placeholder="Position"
+                name="position"
+                />
+                <input type="submit" value="Create Player" />
+            </form>
+            { props.players ? loaded() : loading() }
+        </section>
+    );
+};
+
+export default Show
