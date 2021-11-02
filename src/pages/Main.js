d1596c165dc18159ef5fe5d46b3a64ef38009cc9
vernellmiller <nezeey@gmail.com>
2021-11-02 11:49:14 -0400

    adds state, base url, gets and creates player data

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router";
 import { StyledContent } from "../styles";
 import Chat from "../components/Chat";
@@ -6,13 +7,38 @@ import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 const Content = (props) => {
+
+    const [ players, setPlayers ] = useState(null);
+
+    const BASE_URL = "http://localhost:3001/players/";
+
+    // helper functions
+    const getPlayers = async () => {
+        // const data = await fetch(BASE_URL).then(Response => Response.json());
+        const Response = await fetch(BASE_URL);
+        const data = await Response.json();
+        setPlayers(data);
+    };
+
+    const createPlayers = async (player) => {
+        await fetch(BASE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(player)
+        });
+    };
+
+    useEffect(() => getPlayers(), []);
+
     return (
         <StyledContent>
             <>
                 <h1>Players</h1>
                 <Switch>
                     <Route exact path="/">
-                        <Index /> 
+                        <Index players={players} createPlayers={createPlayers}/> 
                     </Route>
                     <Route path="/players/:id" render={(props) => (
                         <Show {...props} /> 
