1c513b0cdad067853b78060a520a7b558a306f7c
vernellmiller <nezeey@gmail.com>
2021-11-04 20:23:16 -0400

    adds name change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { StyledFooter } from "../styles";
 const Footer = (props) => {
     return (
         <StyledFooter>
-            <p>Copyright &copy; All Rights Reserved Sports Chat {new Date().getFullYear()}</p>
+            <p>Copyright &copy; All Rights Reserved Team Builder {new Date().getFullYear()}</p>
         </StyledFooter>
     );
 };
